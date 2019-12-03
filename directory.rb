5a6a638f120ee57593ef6c007d6e262bf32f6900
pacifica94 <alsayed.bassel@gmail.com>
2019-12-03 18:13:56 +0000

    added control flow to cancel print for empty array of students

--- directory.rb
+++ directory.rb
@@ -2,51 +2,54 @@ def print_header
   puts "The students of Villains Academy".center(50)
   puts "----------------------".center(50)
 end
-# add functionality to choose whether grouping should be by cohort or name selection
-# if it is cohort, create an array of cohorts, and print the users if their hash value is cohort
-def print(names)
-  puts "Would you like to group by cohort or sort by name?"
-  answer = gets.strip!
-  if answer == "cohort"
-    grouped_by_cohort = Hash.new { |h, k| h[k] = [] }
-    names.each { |person| grouped_by_cohort[person[:cohort]] << person }
-    puts grouped_by_cohort
-    print_header
-    c = 0
-    grouped_by_cohort.each_with_index { |(cohort, person), idx| 
 
-      person.each { |i| 
-      puts %{
-        #{(c + 1)}. #{i[:name]} (#{i[:cohort]} cohort)
-           Hobbies: #{i[:hobbies]} 
-           Country: #{i[:country]} 
-           Height: #{i[:height]} cm}.center(50)  
-      c += 1
-    }}
+def print(names)
+  if names.count == 0
+    nil
   else
-    puts "Would you like to print all students or by a specific letter?"
-    selection = gets.capitalize.strip!
-    print_header
-    i = 0
-    until i == names.length
-      if i == names.length 
-        break
-      else
-        if names[i][:name].length < 12
-          if selection != "All" 
-            puts %{
-            #{(i + 1)}. #{names[i][:name]} (#{names[i][:cohort]} cohort)
-              Hobbies: #{names[i][:hobbies]} 
-              Country: #{names[i][:country]} 
-              Height: #{names[i][:height]} cm}.center(50) if names[i][:name][0] == selection
-          else
-            puts %{
-            #{(i + 1)}. #{names[i][:name]} (#{names[i][:cohort]} cohort)
-               Hobbies: #{names[i][:hobbies]} 
-               Country: #{names[i][:country]} 
-               Height: #{names[i][:height]} cm}.center(50)
+    puts "Would you like to group by cohort or sort by name?"
+    answer = gets.strip!
+    if answer == "cohort"
+      grouped_by_cohort = Hash.new { |h, k| h[k] = [] }
+      names.each { |person| grouped_by_cohort[person[:cohort]] << person }
+      puts grouped_by_cohort
+      print_header
+      c = 0
+      grouped_by_cohort.each_with_index { |(cohort, person), idx| 
+
+        person.each { |i| 
+        puts %{
+          #{(c + 1)}. #{i[:name]} (#{i[:cohort]} cohort)
+             Hobbies: #{i[:hobbies]} 
+             Country: #{i[:country]} 
+             Height: #{i[:height]} cm}.center(50)  
+        c += 1
+      }}
+    else
+      puts "Would you like to print all students or by a specific letter?"
+      selection = gets.capitalize.strip!
+      print_header
+      i = 0
+      until i == names.length
+        if i == names.length 
+          break
+        else
+          if names[i][:name].length < 12
+            if selection != "All" 
+              puts %{
+              #{(i + 1)}. #{names[i][:name]} (#{names[i][:cohort]} cohort)
+                Hobbies: #{names[i][:hobbies]} 
+                Country: #{names[i][:country]} 
+                Height: #{names[i][:height]} cm}.center(50) if names[i][:name][0] == selection
+            else
+              puts %{
+              #{(i + 1)}. #{names[i][:name]} (#{names[i][:cohort]} cohort)
+                 Hobbies: #{names[i][:hobbies]} 
+                 Country: #{names[i][:country]} 
+                 Height: #{names[i][:height]} cm}.center(50)
+            end
+            i += 1
           end
-          i += 1
         end
       end
     end
@@ -89,6 +92,7 @@ def input
       puts "Please enter height"
       height = gets.strip!
       students << { name: name.capitalize!, cohort: cohort.to_sym, hobbies: hobbies, country: country.capitalize!, height: height }
+      puts "Now we have #{students.count} students"
       puts "Please enter the names of the student"
       puts "To finish, just hit return twice"
       name = gets.strip!
